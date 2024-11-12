02a61af6a6292723d0f214532666609148070d70
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-11-12 17:45:08 +0000

    Fetch and style

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,44 @@ const App = () => {
     get();
   }, []);
   return (
-    <div>
-      <p>360degrees</p>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'relative',
+      }}
+    >
+      <div
+        style={{
+          width: '50%',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'fixed',
+          top: '0px',
+          left: '0px',
+        }}
+      >
+        <p>360degrees</p>
+      </div>
       {content && (
-        <div style={{ width: '100%' }}>
-          {content.map((image) => {
+        <div
+          style={{
+            width: '50%',
+            display: 'flex',
+            justifyContent: 'flex-end',
+            flexDirection: 'column',
+            position: 'absolute',
+            right: '0px',
+            top: '0px',
+          }}
+        >
+          {content.map((image, index) => {
             return (
               <img
                 style={{ width: '200px', height: '140px' }}
                 key={image.downloadUrl}
                 src={image.downloadUrl}
-                alt="one of them"
+                alt={`The number of this one is ${index + 1}`}
               />
             );
           })}
