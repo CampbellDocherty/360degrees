6664bb750fcab06d5f794b8f0068e01cbd7f9cad
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2024-12-05 14:32:08 +0000

    Hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import { Components } from './Components';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={location.pathname}>
+    <HashRouter basename={location.pathname}>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="components" element={<Components />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
